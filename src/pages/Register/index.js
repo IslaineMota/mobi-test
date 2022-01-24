6363a9b731dafbc3f40ea63d080abf61d979cb4c
IslaineMota <islainemotaoliveira@gmail.com>
2022-01-24 11:26:58 -0300

    style register screen

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import {Btn, Container, ContainForm, Input, Title, Select, Option } from './style'
 
 const useStateWithLocalStorage = localStorageKey => {
     const [values, setValues] = useState(
@@ -32,26 +33,26 @@ function Register() {
       };
 
     return (
-        <>
-            <form onSubmit={submitForm}>
-                <h1>Cadastro de Escolas</h1>
-                 <input type='text' name='school' value={values.school} placeholder='Nome da escola' required onChange={handleOnchange}></input>
-                <input type='text' name='director' value={values.director} placeholder='Nome do diretor' onChange={handleOnchange}></input>
-                <select required onChange={handleOnchange} name='localization'>
-                    <option value={''}></option>
-                    <option value={'1'}>Urbana</option>
-                    <option value={'2'}>Rural</option>
-                </select>
-                <select required onChange={handleOnchange} name='shift'>
-                    <option value={''}></option>
-                    <option value={'M'}>Manhã</option>
-                    <option value={'T'}>Tarde</option>
-                    <option value={'N'}>Noite</option>
-                    <option value={'I'}>Integral</option>
-                </select>
-                <button>Cadastrar</button>
-            </form>
-        </>
+        <Container>
+          <ContainForm onSubmit={submitForm}>
+            <Title>Cadastro</Title>
+            <Input type='text' name='school' value={values.school} placeholder='Nome da escola' required onChange={handleOnchange}></Input>
+            <Input type='text' name='director' value={values.director} placeholder='Nome do diretor' onChange={handleOnchange}></Input>
+            <Select required onChange={handleOnchange} name='localization'>
+              <Option value={''} disabled selectedex>Localidade</Option>
+              <Option value={'1'}>Urbana</Option>
+              <Option value={'2'}>Rural</Option>
+            </Select>
+            <Select required onChange={handleOnchange} name='shift'>
+              <Option value={''} disabled selected>Turno</Option>
+              <Option value={'M'}>Manhã</Option>
+              <Option value={'T'}>Tarde</Option>
+              <Option value={'N'}>Noite</Option>
+              <Option value={'I'}>Integral</Option>
+            </Select>
+            <Btn>Cadastrar</Btn>
+            </ContainForm>
+        </Container>
     )
 }
 
--- src/pages/Register/style.js
+++ src/pages/Register/style.js
@@ -1,3 +1,91 @@
-/* import styled from 'styled-components';
+import styled from 'styled-components';
 
-export const  */
+export const Container = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    overflow: hidden;
+    background: linear-gradient(45deg,#b3e5fc,#9fa8da);
+    font-family: Poppins;
+`;
+
+export const ContainForm = styled.form`
+    display: flex;
+    width: 30vw;
+    align-self: center;
+    flex-direction: column;
+    background: rgba(255, 255, 255, 0.90);
+    padding: 60px 30px;
+    border-radius: 5px;
+    gap: 30px;
+
+    @media screen and (max-width: 1100px) {
+        width: 70vw;
+  }
+`;
+
+export const Title = styled.span`
+    font-size: 28px;
+    font-weight: bold;
+    color: #9fa8da;
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    min-height: 35px;
+    border-radius: 0;
+    border: none;
+    border: 1px solid #B7E6FC;
+    background: none;
+    font-size: 18px;
+    padding: 10px;
+`;
+
+export const Select = styled.select`
+   border-radius: 5px;
+   min-height: 35px; 
+   line-height: 35px; 
+   border: solid 1px #B7E6FC; 
+   background-color: #fff;
+   box-sizing: border-box;
+   clear: both;
+   font-size: 18px;
+   outline: none;
+   padding: 10px;
+}
+`;
+export const Btn = styled.button`
+    height: 36px;
+    min-width: 60px;
+    font-size: 18px;
+    line-height: 27px;
+    text-align: center;
+    background-color: #f8f9fa;
+    border:  1px solid #CFD3EC;
+    border-radius: 4px;
+    color: #3c4043;
+    margin: auto;
+    padding: 0 16px;
+
+    :hover {
+        transition: .5s;
+        background-color: #dae0e5;
+}
+`;
+
+export const Option = styled.option`
+   cursor: pointer;
+   font-weight: 400;
+   line-height: 30px; 
+   min-height: 30px; 
+   list-style: none;
+   outline: none;
+   padding-left: 18px;
+   padding-right: 29px;
+   text-align: left;
+   transition: all 0.2s;
+} 
+`;
